6641d7a16cb42c155f48e89befea8a399a6224b8
mskj-mohao <mohao@mskj.com>
2021-04-12 14:24:47 +0800

    activiti-taskService junit test

--- banana-config/src/test/java/workflow/TestTaskService.java
+++ banana-config/src/test/java/workflow/TestTaskService.java
@@ -1,7 +1,10 @@
 package workflow;
 
+import org.activiti.engine.IdentityService;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
+import org.activiti.engine.identity.Group;
+import org.activiti.engine.task.IdentityLink;
 import org.activiti.engine.task.Task;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -23,13 +27,16 @@ public class TestTaskService {
     private TaskService taskService;
     @Autowired
     private RuntimeService runtimeService;
+    @Autowired
+    private IdentityService identityService;
 
     /**
      * 任务查询
      */
     @Test
     public void queryTaskTest() {
-        Task task = taskService.createTaskQuery().singleResult();
+        Task task = taskService.createTaskQuery().taskId("67506").singleResult();
+        taskService.addCandidateUser("67506","ab1,ab2");
         // 获取全局变量
         Map<String, Object> a = taskService.getVariables(task.getId());
         // 获取局部变量
@@ -38,7 +45,6 @@ public class TestTaskService {
         Map<String, Object> c = runtimeService.getVariables(task.getProcessInstanceId());
         System.currentTimeMillis();
     }
-
     /**
      * 任务签收
      */
@@ -55,5 +61,18 @@ public class TestTaskService {
         Task task = taskService.createTaskQuery().taskAssignee("tester").singleResult();
         taskService.setAssignee(task.getId(),null);
     }
+    /**
+     * 任务完成
+     */
+    @Test
+    public void completeTaskTest() {
+        Task task = taskService.createTaskQuery().taskAssignee("tester").singleResult();
+        taskService.complete(task.getId());
+    }
 
+    @Test
+    public  void qry(){
+        List<IdentityLink> identityLinksForTask = taskService.getIdentityLinksForTask("62506");
+        List<Group> candidate = identityService.createGroupQuery().groupType("candidate").list();
+    }
 }
