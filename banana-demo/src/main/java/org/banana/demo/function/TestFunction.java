33479f00bf41267cdd38cc242407936759834ab5
mohobanana <921873847@qq.com>
2022-04-27 17:26:30 +0800

    package plugin

--- banana-demo/src/main/java/org/banana/demo/function/TestFunction.java
+++ banana-demo/src/main/java/org/banana/demo/function/TestFunction.java
@@ -6,9 +6,6 @@ import org.banana.common.annotation.Function;
 import org.banana.common.annotation.Functions;
 import org.banana.demo.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Functions
 public class TestFunction {
